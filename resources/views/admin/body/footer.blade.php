eac5c730b255dc7713607a02b18da5b40ce82f00
emmanuelrapoo <aniemmanuelrapo@gmail.com>
2022-11-08 08:17:32 +0000

    cleaning admin dashboard

--- resources/views/admin/body/footer.blade.php
+++ resources/views/admin/body/footer.blade.php
@@ -5,9 +5,9 @@
 		<a class="nav-link" href="javascript:void(0)">FAQ</a>
 		</li>
 		<li class="nav-item">
-		<a class="nav-link" href="#">Purchase Now</a>
+		<a class="nav-link" href="#">www.rapoo.com</a>
 		</li>
 	</ul>
 	</div>
-	&copy; 2020 <a href="#">Psd to Html Expert</a>. All Rights Reserved.
+	&copy; 2022 <a href="#">Rapoo Shop</a>. All Rights Reserved.
 </footer>
